e4d4b6279b065e75c40ee1a69bc2198c93179078
victor samson <victorsamson1168@gmail.com>
2020-02-02 13:32:51 +0530

    extra

--- app/src/main/java/com/example/irrigationiot/MainActivity.java
+++ app/src/main/java/com/example/irrigationiot/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.irrigationiot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        finish();
+        Button off=(Button) findViewById(R.id.off);
+        Button on=(Button) findViewById(R.id.on);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("nodes");
+
+
+        off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.setValue("OFF");
+
+
+            }
+        });
+        on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.setValue("ON");
+
+
+            }
+        });
     }
 }
